79e795729a9d9e3fe1070cc4caf91eeb84c0728d
Hui Buwen <bwhui0040@outlook.com>
2020-10-04 03:45:59 +1100

    update firestore

--- UserInfo/UserInfo/AppDelegate.swift
+++ UserInfo/UserInfo/AppDelegate.swift
@@ -16,7 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Use Firebase library to configure APIs
         FirebaseApp.configure()
+        let db = Firestore.firestore()
+        
         return true
     }
 
